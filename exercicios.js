dfc4ff4350e3f70f7205fc5285685975feb2da85
sabrinafsantos <90361542+sabrinafsantos@users.noreply.github.com>
2023-05-01 19:28:39 -0300

    Add files via upload

---
+++ exercicios.js
@@ -0,0 +1,23 @@
+/* Uma função que escreve o nome 
+
+function escrevaSeuNome (nome){
+    console.log ('Meu nome é: ' + nome);
+
+}
+
+escrevaSeuNome('Sabrina');
+escrevaSeuNome('Jefferson');
+escrevaSeuNome('Roberto');
+*/
+
+/* Uma função que vê se é maior de idade */
+
+function maiorIdade (idade) {
+    if(idade < 18){
+        console.log('Você é não é maior de idade');
+    } else {
+        console.log('Você é maior de idade');}
+    } 
+
+maiorIdade(58);
+
